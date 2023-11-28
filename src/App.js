36ca5c7c3fcc28eff970d61d7aeae76ed4ce2e82
nrs20 <nrs5@njit.edu>
2023-11-28 15:19:59 -0500

    now i only need to sign in once

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import jwt_decode, { jwtDecode } from "jwt-decode";
 
 function App() {
@@ -6,79 +6,90 @@ function App() {
 
   const [user, setUser] = useState({});
   const [accessToken, setAccessToken] = useState(null);
+  //const [oauthCalled, setOauthCalled] = useState(false); // Initialize with false
+  const [oauthCalled, setOauthCalled] = useState(() => {
+    // Initialize from localStorage or default to false
+    return JSON.parse(localStorage.getItem("oauthCalled")) || false;
+  });
 
-  const handleCallbackResponse = async (response) => {
-    console.log("Encoded JWT ID Token: " + response.credential);
-    const userObject = jwtDecode(response.credential);
-    console.log(userObject);
-    console.log(userObject.email);
-    const calendarId = userObject.email;
-
-  const searchParams = new URLSearchParams(window.location.hash.substring(1)); // Exclude the leading '#'
-  const accessToken = searchParams.get("access_token");
-
-    if (accessToken) {
-      console.log("Access Token:", accessToken);
-      try {
-        const response = await fetch(`https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events?access_token=${accessToken}`);
-        const data = await response.json();
-        console.log("HI");
-        console.log("PRINT SOMETHING DAMMIT!!!")
-        console.log("THIS IS THE RESPONSE " , response);
-        console.log("THIS IS THE DATA " , data);
-        listUpcomingEvents(data.items); // Set the events state
-
-      }
-      catch (e) {
-        console.log("ERROR");
-        console.log(e);
-      }
-
-
-    setAccessToken(accessToken);
-  } else {
-    console.log("No Access Token");
-  }
-
-    /*
-    const accessTokenRegex = /access_token=([^&]+)/;
-    console.log("Access Token: " + accessTokenRegex)
-    const match = window.location.hash.match(accessTokenRegex);
-    console.log("Access Token: " + match)
-
-    if (match && match[1]) {
-      setAccessToken(match[1]);
-    }
-*/
-//const searchParams = new URLSearchParams(window.location.search);
-const authorizationCode = searchParams.get("code");
-console.log("Authorization Code: " + authorizationCode);
-    document.getElementById("signInDiv").hidden = true;
-  };
 
   const handleSignOut = (event) => {
     setUser({});
     setAccessToken(null);
+    setOauthCalled(false); // Reset to false when signing out
+
     document.getElementById("signInDiv").hidden = false;
   };
 
-  useEffect(() => {
-    /* global google */
-    google.accounts.id.initialize({
-      client_id: "150401460223-dpijoj0c3f8qqbref8j00kqqbn460qgf.apps.googleusercontent.com",
-      callback: handleCallbackResponse,
-    });
 
-    google.accounts.id.renderButton(
-      document.getElementById("signInDiv"),
-      { theme: "outline", size: "large", shape: "rectangular" }
-    );
-    google.accounts.id.prompt();
-  }, []);
-/*
+
+const handleCallbackResponse = async (response) => {
+  console.log("User's email:", response.email);
+  const calendarId = response.email;
+
+  // ... rest of your code
+};
+  //instead of signing in multiple times, just sign in once and then use the access token to get the calendar events
+  useEffect(() => {
+    const fetchData = async () => {
+      if (oauthCalled) {
+        const searchParams = new URLSearchParams(window.location.hash.substring(1));
+        const accessToken = searchParams.get("access_token");
+  
+        if (accessToken) {
+          try {
+            // Fetch user's email from the UserInfo endpoint
+            const emailSite = `https://www.googleapis.com/oauth2/v2/userinfo?access_token=${accessToken}`;
+            const emailFetch = await fetch(emailSite);
+            const emailData = await emailFetch.json();
+            const userEmail = emailData.email;
+  
+            // Log and store the email
+            console.log("User's email:", userEmail);
+  
+            // Make the Google Calendar API request
+            const calendarApiUrl = `https://www.googleapis.com/calendar/v3/calendars/${userEmail}/events?access_token=${accessToken}`;
+            const response = await fetch(calendarApiUrl);
+            const data = await response.json();
+  
+            // Log and process the Google Calendar API response
+            console.log("Google Calendar API Response:", data);
+            listUpcomingEvents(data.items); // Set the events state
+            handleCallbackResponse({ email: userEmail });
+  
+          } catch (e) {
+            console.log("Error:", e);
+          }
+          setAccessToken(accessToken);
+          // Do whatever you want with the access token here
+        } else {
+          console.log("NO ACCESS TOKEN");
+        }
+      }
+    };
+  
+    fetchData();
+  }, [oauthCalled]);
+
+  const saveOauthCalledToStorage = (value) => {
+    setOauthCalled(value);
+    localStorage.setItem("oauthCalled", JSON.stringify(value));
+  };
+  /*
  * Create form to request access token from Google's OAuth 2.0 server.
  */
 function oauthSignIn() {
+  //setOauthCalled(true); // Update the state to true
+  saveOauthCalledToStorage(true);
+
+  if(oauthCalled){
+    console.log("OAUTH CALLED IN OAUTH SIGN IN");
+
+    
+  }
+  else{
+    console.log("OAUTH NOT CALLED IN OAUTH SIGN IN");
+  }
   // Google's OAuth 2.0 endpoint for requesting an access token
   var oauth2Endpoint = 'https://accounts.google.com/o/oauth2/v2/auth';
 
@@ -88,15 +99,14 @@ function oauthSignIn() {
   form.setAttribute('action', oauth2Endpoint);
 
   // Parameters to pass to OAuth 2.0 endpoint.
-  var params = {'client_id': '150401460223-dpijoj0c3f8qqbref8j00kqqbn460qgf.apps.googleusercontent.com',
-                'redirect_uri': 'http://localhost:3000',
-                'response_type': 'token',
-                
-                'scope': 'https://www.googleapis.com/auth/calendar', // Add the calendar scope here
-
-                //'scope': 'https://www.googleapis.com/auth/drive.metadata.readonly',
-                'include_granted_scopes': 'true',
-                'state': 'pass-through value'};
+  var params = {
+    'client_id': '150401460223-dpijoj0c3f8qqbref8j00kqqbn460qgf.apps.googleusercontent.com',
+    'redirect_uri': 'http://localhost:3000',
+    'response_type': 'token',
+    'scope': 'https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/userinfo.email',
+    'include_granted_scopes': 'true',
+    'state': 'pass-through value'
+  };
 
   // Add form parameters as hidden input values.
   for (var p in params) {
@@ -109,49 +119,25 @@ function oauthSignIn() {
 
   // Add form to page and submit it to open the OAuth 2.0 endpoint.
   document.body.appendChild(form);
+ // console.log("DONE");
+
   form.submit();
-
 }
 
-/*
-  return (
-    <div className="App">
-      <div id="signInDiv"></div>
 
-      {accessToken && (
-        <div>
-          <p>Access Token: {accessToken}</p>
-          <button onClick={(e) => handleSignOut(e)}>Sign Out</button>
-        </div>
-      )}
-
-      {user && (
-        <div>
-          <img src={user.picture} alt="User"></img>
-          <h3>{user.name}</h3>
-        </div>
-        
-      )}
-      
-            <button onClick={oauthSignIn}>Sign In with Google</button>
-
-    </div>
-  );
-}
-*/
 
 
 
 
 const listUpcomingEvents = (eventsData) => {
+  console.log("Setting events:", eventsData);
   setEvents(eventsData);
 };
 return (
   <div className="App">
     {accessToken ? (
       <div>
-        <p>Access Token: {accessToken}</p>
-        <button onClick={handleSignOut}>Sign Out</button>
+                <button onClick={handleSignOut}>Sign Out</button>
         <h1>Google Calendar API Example</h1>
         <ul>
          
